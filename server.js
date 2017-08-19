27af35988f2c3143f478f692ba32724e135d55b9
raphaelraj1035 <raphael1035@live.com>
2017-08-20 02:55:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,11 +21,11 @@ app.get('/artical-one', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'artical-one.html'));
 });
 
-app.get('/artical-one', function (req, res) {
+app.get('/artical-two', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'artical-two.html'));
 });
 
-app.get('/artical-one', function (req, res) {
+app.get('/artical-three', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'artical-three.html'));
 });
 
