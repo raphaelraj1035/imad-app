30c4b66493abed49a0929bb24f3427e758183562
raphaelraj1035 <raphael1035@live.com>
2017-08-21 19:24:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var content = {
+var articalOne = {
     title: 'Artical One | Raphael',
     heading: 'Artical One',
     date: 'Sep 5, 2016',
