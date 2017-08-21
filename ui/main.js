b723835ef1357a4eae017fa3142b855976e93b31
raphaelraj1035 <raphael1035@live.com>
2017-08-22 03:17:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 
 var ele = document.getElementById('main-text');
 ele.innerHTML = 'RAPH';
+
+var img = document.getElementById('modi');
+log.onclick = function () {
+    img.style.marginleft = '100px';
+};
