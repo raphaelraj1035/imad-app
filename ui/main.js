38514e2a6156718c69e3e4af9e58df96b32919ce
raphaelraj1035 <raphael1035@live.com>
2017-08-22 04:31:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,20 @@
 var button = document.getElementById('countet');
 var counter = 0;
 button.onclick = function () {
-  counter = counter + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
+    
+    // Make a request to the counter endpoint
+    var request = new XMLHttpRequest();
+    
+    // Capture the response and store it in a variable
+    
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.Done){
+            // Take some action
+            if (request.status ===200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
 };
